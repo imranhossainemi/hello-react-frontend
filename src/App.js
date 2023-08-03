998adc59650ae44e0a03d33474ad94e2024cc794
Imran Hossain <blur-imran@hotmail.com>
2023-08-03 17:26:40 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import { useDispatch } from 'react-redux';
-import React, { useEffect } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Greeting from './components/Greetings';
-import { FetchGreeting } from './redux/greetingSlice';
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(FetchGreeting());
-  }, [dispatch]);
-
   return (
     <div className="App">
       <Routes>
